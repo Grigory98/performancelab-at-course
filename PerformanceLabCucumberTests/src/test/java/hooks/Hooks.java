c05f891ee1991d03849f6241cbc9ade75ad9545a
Grigory98 <yakimov_ga@mail.ru>
2023-07-26 23:38:04 +0400

    Добавление скриншота

--- PerformanceLabCucumberTests/src/test/java/hooks/Hooks.java
+++ PerformanceLabCucumberTests/src/test/java/hooks/Hooks.java
@@ -2,7 +2,13 @@ package hooks;
 
 import core.DriversManager;
 import io.cucumber.java.After;
+import io.cucumber.java.Scenario;
+import io.qameta.allure.Allure;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import steps.Steps;
+
+import java.io.ByteArrayInputStream;
 import java.util.HashMap;
 
 public class Hooks extends Steps {
@@ -13,8 +19,16 @@ public class Hooks extends Steps {
     }
     //endregion
 
-    @After
-    public void tearEach()
+    @After(order = 1)
+    public void takeScraenshotOnFailure(Scenario scenario)
+    {
+        TakesScreenshot ts = (TakesScreenshot) DriversManager.getCurrent();
+        byte[] src = ts.getScreenshotAs(OutputType.BYTES);
+        scenario.attach(src, "image/png", "screenshot");
+    }
+
+    @After(order = 0)
+    public void tearDown()
     {
         DriversManager.closeBrowser();
     }
