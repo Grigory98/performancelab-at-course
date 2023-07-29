bf15fb9e51ced33adbf35ccb0977b9c19ed705b6
Grigory98 <yakimov_ga@mail.ru>
2023-07-29 22:24:14 +0400

    убрал двоение тестов

--- PerformanceLabCucumberTests/src/test/java/hooks/Hooks.java
+++ PerformanceLabCucumberTests/src/test/java/hooks/Hooks.java
@@ -3,33 +3,21 @@ package hooks;
 import core.DriversManager;
 import io.cucumber.java.After;
 import io.cucumber.java.Scenario;
-import io.qameta.allure.Allure;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
-import steps.Steps;
+import java.io.IOException;
 
-import java.io.ByteArrayInputStream;
-import java.util.HashMap;
-
-public class Hooks extends Steps {
-
-    //region Конструктор
-    public Hooks(HashMap<String, Object> scContext) {
-        super(scContext);
-    }
-    //endregion
-
-    @After(order = 1)
-    public void takeScraenshotOnFailure(Scenario scenario)
-    {
-        TakesScreenshot ts = (TakesScreenshot) DriversManager.getCurrent();
-        byte[] src = ts.getScreenshotAs(OutputType.BYTES);
-        scenario.attach(src, "image/png", "screenshot");
-    }
+public class Hooks {
 
     @After(order = 0)
-    public void tearDown()
+    public void tearDown(Scenario scenario) throws InterruptedException, IOException, IllegalMonitorStateException
     {
+        if(scenario.isFailed()) {
+            TakesScreenshot ts = (TakesScreenshot) DriversManager.getCurrent();
+            byte[] src = ts.getScreenshotAs(OutputType.BYTES);
+            scenario.attach(src, "image/png", "screenshot");
+        }
+
         DriversManager.closeBrowser();
     }
 }
